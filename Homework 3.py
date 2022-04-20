9df5a62e3c46e1c21c9bdf72cc5430d480b11097
franceswireko <w1782829@my.westminster.ac.uk>
2022-04-20 13:03:10 +0100

    removed idea and DS Store folder

---
+++ Homework 3.py
@@ -0,0 +1,15 @@
+
+#Question 1
+weather_forecast = input('Is it raining? y/n ')
+if weather_forecast == 'y':
+    print('Take an umbrella')
+else:
+    print('You dont need an umbrella')
+
+#Question 2
+my_money = input('How much money do you have?')
+boat_cost = 20 +5
+if my_money < boat_cost:
+    print('You can afford the boat hire')
+else:
+    print('You cannot afford the board hire')
