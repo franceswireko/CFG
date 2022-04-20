2e6e1f88a5fc08b9c636e35f8022c50489110a12
franceswireko <w1782829@my.westminster.ac.uk>
2022-04-20 11:31:08 +0100

    removed idea and DS Store folder

---
+++ Homework 2.py
@@ -0,0 +1,16 @@
+# Question 1
+for number in range(100):
+    output = 'o' * number
+    print(output)
+
+# Answer:
+# The program above prints out the letter 'o'
+# in incremental order 100 times
+
+
+# Question 2
+def calculate_vat(amount):
+    return amount * 1.2  #added return function
+total = calculate_vat(100)
+print("Total:", total)
+
